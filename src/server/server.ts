a9d48ab5229014d8ee102227a08c02b3ec28d9e6
Eric Opoku <ericus20@erics-mbp.mynetworksettings.com>
2023-08-24 20:23:52 -0400

    feat: setup mongo db

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,6 @@
 import cors from "cors"
 import dotenv from "dotenv"
 import express from "express"
-import mongoose from "mongoose"
 import { router1 } from "./routers"
 
 dotenv.config()
@@ -17,15 +16,32 @@ app.use(
 )
 app.use(router1)
 
-export const db = async () => {
-	try {
-		const connection = await mongoose.connect(process.env.MONGODB_URL as string)
-		console.log(`mongodb connected: ${connection.connection.host}`)
-		// const data = await db.collection('menu').find().toArray();
-	} catch (error) {
-		console.error(error)
-	}
+import { MongoClient, ServerApiVersion } from 'mongodb'
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4e0jjnt.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+console.log("url", uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+run().catch(console.dir);
 // await db()
 
 // store this on MongoDB
